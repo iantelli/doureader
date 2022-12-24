12a355180846fbe8726e4bba3ffeccd06f452a3e
iantelli <2ndex@duck.com>
2022-12-23 19:31:31 -0800

    :lipstick: Add image component

---
+++ src/components/atoms/image.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import { twMerge } from "tailwind-merge"
+
+type ImageProps = {
+  src: string
+  alt?: string
+  width: number
+  height: number
+  className?: string
+}
+
+export default function ({ src, alt, width, height, className }: ImageProps) {
+  return <Image src={src} alt={alt} width={width} height={height} className={twMerge("rounded", className ?? "")} />
+}
