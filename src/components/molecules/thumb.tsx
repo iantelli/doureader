15f1c58b10a50b06290760cff3fa1a8322dc9760
iantelli <2ndex@duck.com>
2022-12-27 16:14:03 -0800

    :lipstick: Update blur hover effect

--- src/components/molecules/thumb.tsx
+++ src/components/molecules/thumb.tsx
@@ -28,24 +28,21 @@ export default function ({ src, doujin }: ThumbProps) {
     <Link href={`/gallery/${doujin.id}`}>
       <div className="relative">
         <Image src={src} alt={doujin.title.pretty} width={"w-72"} className={"inset-0 z-0 rounded-md m-2"} />
-
-        <div className="w-72 ml-2 p-3 opacity-0 hover:opacity-100 hover:backdrop-blur-sm backdrop-grayscale duration-500 rounded-md absolute inset-0">
+        <div className="w-72 ml-2 p-3 opacity-0 hover:opacity-100 bg-black/90 duration-300 rounded-md absolute inset-0">
           <Image
             src={checkCountry(
               doujin.tags.filter((tag) => tag.type === "language").filter((tag) => tag.name !== "translated")[0].name
             )}
             width={"w-6"}
           />
-          <HeaderText textSize="text-2xl" textColor="text-rose-600">
-            {doujin.title.pretty.slice(0, 20) + "..."}
-          </HeaderText>
+          <HeaderText textSize="text-2xl">{doujin.title.pretty}</HeaderText>
           <div className="absolute bottom-0 right-0 p-3">
-            <SubHeaderText textSize={"text-md"} textColor="text-rose-500">
+            <SubHeaderText textSize={"text-md"} textColor="text-zinc-400">
               {moment(doujin.upload_date * 1000).fromNow()}
             </SubHeaderText>
           </div>
           <div className="absolute bottom-0 left-0 p-3">
-            <SubHeaderText textSize={"text-lg"} textColor="text-rose-500">
+            <SubHeaderText textSize={"text-lg"} textColor="text-zinc-400">
               {doujin.num_pages} pages
             </SubHeaderText>
           </div>
